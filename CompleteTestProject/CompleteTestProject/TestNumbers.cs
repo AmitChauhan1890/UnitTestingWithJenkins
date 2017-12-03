771961fb9deb0851f6f70a9343e56ec8f696ae0f
amitchauhan101 <amit.chauhan@monotype.com>
2017-12-03 11:22:13 +0530

    initial code commit

---
+++ CompleteTestProject/CompleteTestProject/TestNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompleteTestProject
+{
+    [TestClass]
+    public class TestNumbers
+    {
+        [TestMethod]
+        public void TestPrimeNumber()
+        {
+            Assert.IsTrue(isPrimeNumber(5), "Entered number is not prime number.");
+        }
+
+        [TestMethod]
+        public void TestPrimeNumber2()
+        {
+            Assert.IsTrue(isPrimeNumber(6), "Entered number is not prime number.");
+        }
+
+        [TestMethod]
+        public void TestNumberEquality()
+        {
+            Assert.AreEqual(10, 10.0, "Entered numbers are not equal.");
+        }
+
+        private Boolean isPrimeNumber(int number)
+        {
+            bool flag = true;
+            for(int i=2; i<number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    flag = false;
+                    return flag;
+                }
+            }
+            return flag;
+        }
+    }
+}
---
+++ CompleteTestProject/CompleteTestProject/TestString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompleteTestProject
+{
+    [TestClass]
+    public class TestString
+    {
+        [TestMethod]
+        public void TestEqual()
+        {
+            Assert.AreEqual("HELLO", "HELLO", "Strings are not equal.");
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            Assert.IsTrue("Hello India".Contains("Hello"), "Enter word doesn't found in String.");
+        }
+    }
+}
